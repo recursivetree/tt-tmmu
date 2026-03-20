4ef2c13b665e98206c2677481fc446688ae82aef
recursivetree <recursivetreemail@gmail.com>
2026-03-21 00:14:36 +0100

    quick implementation of the system

--- src/tmmu.sv
+++ src/tmmu.sv
@@ -3,7 +3,9 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-module tmmu (
+module tmmu #(
+    parameter integer PAGE_NUMBER_BITS = 8
+) (
     input logic clk_i,
     input logic rst_ni,
 
@@ -13,13 +15,35 @@ module tmmu (
     input logic we_i,
     output logic ready_o
 );
-    typedef enum logic [1:0] {TRANSLATING, ACQUIRE, WRITE} state_t;
+    /* latch memory */
+    logic write_now_q, write_now_d;
+    logic [PAGE_NUMBER_BITS-1:0] captured_vaddr_q, captured_vaddr_d;
+    logic [PAGE_NUMBER_BITS-1:0] captured_new_vaddr_q, captured_new_vaddr_d;
 
+    logic [PAGE_NUMBER_BITS-1:0][PAGE_NUMBER_BITS-1:0] memory_q;
+    logic [PAGE_NUMBER_BITS-1:0] vpage_d;
+    logic [8-PAGE_NUMBER_BITS-1:0] voffset_d;
+
+    assign vpage_d = vaddr[7:7-PAGE_NUMBER_BITS+1];
+    assign voffset_d = vaddr[PAGE_NUMBER_BITS-1:0];
+    assign paddr_o = {memory_q[vpage_d], voffset_d};
+
+    always_latch begin
+        if(write_now_q && clk_i) begin
+            memory_q[captured_vaddr_q] = captured_new_vaddr_q;
+        end
+    end
+
+    /* state machine */
+    typedef enum logic [1:0] {TRANSLATING, ACQUIRE, WRITE} state_t;
     state_t state_q, state_d;
 
     always_comb begin
         state_d = state_q;
         ready_o = 0;
+        write_now_d = 0;
+        captured_vaddr_d = captured_vaddr_q;
+        captured_new_vaddr_d = captured_new_vaddr_q;
 
         case (state_q)
             TRANSLATING: begin
@@ -28,10 +52,13 @@ module tmmu (
 
                 if (we_i) begin
                     state_d = ACQUIRE;
+                    captured_vaddr_d = vaddr_i[7:7-PAGE_NUMBER_BITS+1];
                 end
             end
             ACQUIRE: begin
                 state_d = WRITE;
+                write_now_d = 1; // we have to trigger it a cycle before we need to use the value
+                captured_new_vaddr_d = vaddr_i[7:7-PAGE_NUMBER_BITS+1];
             end
             WRITE: begin
                 state_d = TRANSLATING;
@@ -50,8 +77,12 @@ module tmmu (
     always_ff @(posedge clk_i or negedge rst_ni) begin
         if (!rst_ni) begin
             state_q <= TRANSLATING;
+            write_now_q <= 0;
         end else begin
             state_q <= state_d;
+            write_now_q <= write_now_d;
+            captured_vaddr_q <= captured_vaddr_d;
+            captured_new_vaddr_q <= captured_new_vaddr_d;
         end
     end
 
